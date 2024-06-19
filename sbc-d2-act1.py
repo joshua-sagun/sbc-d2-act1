feb9c0bca2885b4d54f39145ccfeab3cb8ba79e7
joshua_sagun <joshua.sagun@aclcbutuan.edu.ph>
2024-06-19 12:29:39 +0800

    uswa

---
+++ sbc-d2-act1.py
@@ -0,0 +1,13 @@
+lab = "summer bootcamp"
+
+jay1=lab.title()
+print(jay1)
+jay2 = lab.capitalize()[:-1] + lab[-1].upper()
+print(jay2)
+letter = "L"
+jay3 = print(f"{letter}{lab[8:11]}")
+print(jay3)
+print(lab[-4:-1]+"e")
+print(f"{lab[-3].capitalize()}{lab[5].capitalize()}")
+oha = lab.replace(" " ,"")
+print(oha.isalpha())
